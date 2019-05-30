ca4ece79af3853c6b255da3d68afc73d661fb247
priyanka <prinegi100@gmail.com>
2019-05-30 13:03:43 +0530

    added clicker component to display random num

---
+++ src/clicker.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Clicker extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            num : 1
+        }
+    }
+
+    handleEvent = () => {
+        const randomNumber = Math.floor(Math.random()*10)+1;
+        this.setState({num:randomNumber});
+    }
+
+    render() {
+        return (<div >
+                    <h1>Number is {this.state.num} </h1>
+                    {this.state.num === 7 && <h2>You Win!</h2> }
+                    <button onClick = {this.handleEvent}>Random Number</button>
+                </div>)
+    }
+    
+}
+
+export default Clicker;
