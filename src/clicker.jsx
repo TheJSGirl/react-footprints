cdc37c9e535775f69e973b89b14c833a978d4e4a
priyanka <prinegi100@gmail.com>
2019-05-30 14:19:30 +0530

    added css for button & div elements

--- src/clicker.jsx
+++ src/clicker.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import './Clicker.css';
 
 class Clicker extends Component {
 
@@ -16,9 +17,11 @@ class Clicker extends Component {
     }
 
     render() {
-        return (<div >
-                    <h1>Number is {this.state.num} </h1>
-                    {this.state.num === 7 ? <h2>You Win!</h2> : <button onClick = {this.handleEvent}>Random Number</button>}
+        return (<div className="Clicker">
+                    <div className="Clicker-div">
+                    <h1>Number is {this.state.num} </h1> &nbsp;&nbsp;&nbsp;&nbsp;
+                    {this.state.num === 7 ? <h2 className="Clicker-win">You Win!</h2> : <button className="Clicker-btn" onClick = {this.handleEvent}>Random Number</button>}
+                    </div>
                 </div>)
     }
     
