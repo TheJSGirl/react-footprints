d1464cdb7053bab0fb081ba0bbc984d950816192
TheJSGirl <prinegi100@gmail.com>
2019-06-06 00:04:25 +0530

    used Route & BrowerRouter for routing

--- react-rounting/src/App.js
+++ react-rounting/src/App.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
+
 import Dog from './components/Dog/Dog';
 import Contact from './components/Contact/Contact';
 import About from './components/About/About';
 
 class App extends Component {
 
-  
+
   render() {
     return (
       <div className="App">
- 
+        <Route path="/dog" component={Dog}/>
+        <Route path="/" component={About}/>
+        <Route path="/contact" component={Contact}/>
     </div>
     )
   }
