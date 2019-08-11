7edadf989f243806bd4a7457e59fa0694df7fd5b
thejsgirl <prinegi100@gmail.com>
2019-08-11 13:42:35 +0530

    feat: reusability of useState hook

--- react-hooks/src/components/toggler.jsx
+++ react-hooks/src/components/toggler.jsx
@@ -2,11 +2,19 @@ import React, {useState} from 'react';
 
 function Toggler() {
     const [isHappy, setIsHappy] = useState(true);
+    const [isAngry, setIsAngry] = useState(false);
+
     const toggle = () => {
         setIsHappy(!isHappy);
     }
+    const toggleisAngry = () => {
+        setIsAngry(!isAngry)
+    }
     return (
-        <h1 onClick={toggle}>{isHappy ? "🙂🙂🙂🙂🙂🙂🙂" : "😔😔😔😔😔😔😔"}</h1>
+        <div>
+            <h1 onClick={toggle}>{isHappy ? "🙂🙂🙂🙂🙂🙂🙂" : "😔😔😔😔😔😔😔"}</h1>
+            <h1 onClick={toggleisAngry}>{isAngry ? "😡😡😡😡😡😡" : " 🤗 🤗 🤗 🤗 🤗 🤗"}</h1>
+        </div>
     )
 }
 
