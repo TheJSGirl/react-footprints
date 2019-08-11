eab0855a806fa206a4c9cc47595d7616ee2c0543
thejsgirl <prinegi100@gmail.com>
2019-08-11 13:24:10 +0530

    feat: used useState hook

--- react-hooks/src/components/counter.jsx
+++ react-hooks/src/components/counter.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
 function CounterHooks() {
+    const [count, setCount] = useState(0);
+    
     return (
         <div>
-            <h1>The Count is:</h1>
-            <button>Add 1</button>
+            <h1>The Count is: {count}</h1>
+            <button onClick={ () => setCount(count + 1)}>Add 1</button>
         </div>
     )
 }
