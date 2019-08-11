5e4c362cfc48455ff96e7512784a8027f080c2eb
thejsgirl <prinegi100@gmail.com>
2019-08-11 13:34:58 +0530

    feat: created toggle component
    Made toggle component for happy & sad emojis

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Counter from './components/counter';
+import Toggler from './components/toggler';
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <Toggler />
     </div>
   );
 }
---
+++ react-hooks/src/components/toggler.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+function Toggler() {
+    const [isHappy, setIsHappy] = useState(true);
+    const toggle = () => {
+        setIsHappy(!isHappy);
+    }
+    return (
+        <h1 onClick={toggle}>{isHappy ? "🙂🙂🙂🙂🙂🙂🙂" : "😔😔😔😔😔😔😔"}</h1>
+    )
+}
+
+export default Toggler;
