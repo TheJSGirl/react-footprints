733aacb1801cb2bea2c2581adb58aea3c6ba17c7
Priyanka <prinegi100@gmail.com>
2019-08-17 08:46:41 +0530

    feat: added react hooks form

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Counter from './components/counter';
 import Toggler from './components/toggler';
+import Form from './components/form';
 
 function App() {
   return (
     <div className="App">
       <Counter />
       <Toggler />
+      <Form />
     </div>
   );
 }
