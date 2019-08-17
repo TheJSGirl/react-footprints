7da90a894eb394229a57b62472e64692dcabee9b
Priyanka <prinegi100@gmail.com>
2019-08-17 09:32:38 +0530

    feat: added multiple input for same form hook

--- react-hooks/src/components/customForm.jsx
+++ react-hooks/src/components/customForm.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import useFormState from '../hooks/useFormState';
 
 function SimpleFormHooks() {
-  const [email, handleOnchange, reset] = useFormState("");
+  const [email, updateEmail, resetEmail] = useFormState("");
+  const [password, updatePassword, resetPassword] = useFormState("");
 
   return (<div>
-    <input type="text" value={email} onChange={handleOnchange}/>
-    <button onClick={reset}>Submit</button>
+    <input type="text" value={email} onChange={updateEmail}/>
+    <input type="password" value={password} onChange={updatePassword} />
+    <button onClick={resetEmail}>Submit</button>
+    <button onClick={resetPassword}>Reset Password</button>
   </div>)
 }
 
