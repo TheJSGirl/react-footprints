5f38c1c55394eed1fccdf93d28a5ab502ceb851f
Priyanka <prinegi100@gmail.com>
2019-09-14 11:25:53 +0530

    feat: added inner html

--- react-hooks/src/components/clicker.jsx
+++ react-hooks/src/components/clicker.jsx
@@ -1,7 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Clicker() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `You have Clicked ${count} times`;
+  })
   return (
     <button onClick={() => setCount(count+1)}>Click Me {count}</button>
   )
