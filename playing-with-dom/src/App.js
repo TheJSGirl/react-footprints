cab0ca7dc10831cccba1cad21c1becd0f671c165
TheJSGirl <prinegi100@gmail.com>
2019-11-14 15:54:06 +0530

    feat: added axios & build search component

--- playing-with-dom/src/App.js
+++ playing-with-dom/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import axios from 'axios';
 
-function App() {
+function App(props) {
+
+  const onSearchTerm = (term) => {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term},
+      headers: {
+        Authorization: 'Client-ID de669458b5837bacb9765a660efcbd8e02609e1e7d1edd6ba88fd37cabe04b77'
+      }
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container" style={{marginTop:"10px"}}>
+      <SearchBar onSearchTerm={onSearchTerm} />
     </div>
   );
 }
---
+++ playing-with-dom/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    state = {
+        term: ''
+    };
+
+    onInputChange = (event) => {
+      this.setState({term: event.target.value});  
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSearchTerm(this.state.term);
+    }
+
+    render(){
+        return (
+            <div className="ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" 
+                        value={this.state.term}
+                        onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
