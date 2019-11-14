b65abddadaa2d6fd270453bc5b27f1ea7c5d0e99
TheJSGirl <prinegi100@gmail.com>
2019-11-14 23:10:03 +0530

    feat: made request for search term

--- playing-with-dom/src/App.js
+++ playing-with-dom/src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import axios from 'axios';
 
 function App(props) {
+  const [image, addImages] = useState([]);
 
-  const onSearchTerm = (term) => {
-    axios.get('https://api.unsplash.com/search/photos', {
+  const onSearchTerm = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term},
       headers: {
         Authorization: 'Client-ID de669458b5837bacb9765a660efcbd8e02609e1e7d1edd6ba88fd37cabe04b77'
       }
     })
+    addImages(response.data.results);
   }
+
   return (
     <div className="ui container" style={{marginTop:"10px"}}>
       <SearchBar onSearchTerm={onSearchTerm} />
+      Found: {image.length} images
     </div>
   );
 }
