3e2d4e35f11ab757e33bff7e4c614e196dfddfdb
TheJSGirl <prinegi100@gmail.com>
2019-12-01 19:32:37 +0530

    feat: created  imageRef for clientHeight

--- playing-with-dom/src/components/ImageCard/ImageCard.jsx
+++ playing-with-dom/src/components/ImageCard/ImageCard.jsx
@@ -2,12 +2,23 @@ import React, {Component} from 'react';
 import './ImageCard.css';
 
 class ImageCard extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
      render() {
          const {description, urls} = this.props.image;
         return (
             <div>
-                <img className="image" alt={description}
+                <img ref={this.imageRef} className="image" alt={description}
                 src={urls.regular}
                 />
             </div>
