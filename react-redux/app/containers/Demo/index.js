772bf1f54d4bd0703c354feeb0adbc75f46b3345
TheJSGirl <prinegi100@gmail.com>
2020-01-05 15:25:24 +0530

    feat: validate the form

--- react-redux/app/containers/Demo/index.js
+++ react-redux/app/containers/Demo/index.js
@@ -7,24 +7,33 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class DemoPage extends Component {
-  renderInput(formProps) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return <div>{error}</div>;
+    }
+  }
+
+  renderInput = formProps => (
     // return (
     //   <input
     //     onChange={formProps.input.onChange}
     //     value={formProps.input.value}
     //   />
     // );
-    return (
-      <div>
-        <label>{formProps.label}</label>
-        <input {...formProps.input} />
-      </div>
-    );
+    <div>
+      <label>{formProps.label}</label>
+      <input {...formProps.input} />
+      {this.renderError(formProps.meta)}
+    </div>
+  );
+
+  onSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <Field name="title" component={this.renderInput} label="Enter title" />
         <Field
           name="description"
@@ -36,6 +45,19 @@ class DemoPage extends Component {
   }
 }
 
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+  return errors;
+};
+
 export default reduxForm({
   form: 'DemoForm',
+  validate,
 })(DemoPage);
