46cdbbf1164a29574fb9bafa29baefbf33b50deb
TheJSGirl <prinegi100@gmail.com>
2020-01-05 19:15:46 +0530

    refactor: minor change

--- react-redux/app/containers/App/index.js
+++ react-redux/app/containers/App/index.js
@@ -14,7 +14,6 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
 import Footer from 'components/Footer';
 import ReduxForm from 'containers/Demo/Loadable';
 
@@ -32,13 +31,12 @@ const AppWrapper = styled.div`
 export default function App() {
   return (
     <AppWrapper>
-      {/* <Helmet
+      <Helmet
         titleTemplate="%s - React.js Boilerplate"
         defaultTitle="React.js Boilerplate"
       >
         <meta name="description" content="A React.js Boilerplate application" />
-      </Helmet> */}
-      {/* <Header /> */}
+      </Helmet>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/form" component={ReduxForm} />
