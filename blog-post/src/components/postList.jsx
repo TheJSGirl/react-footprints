993f4ec75e670df5d59c818873c8cdc62f82dcd3
Priyanka Negi <prinegi100@gmail.com>
2020-07-20 00:16:04 +0530

    initial commit

---
+++ blog-post/src/components/postList.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import axios from 'axios';
+
+
+const PostList = () => {
+    useEffect(() => {
+        
+    })
+    return <div>Hey</div>
+}
+
+export default PostList;
